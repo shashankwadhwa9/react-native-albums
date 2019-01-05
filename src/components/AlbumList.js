4e5249ac1eec0da9540c4e21734029c8e2bc68c8
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-05 13:30:32 +0530

    change state when we get the response

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,12 +3,18 @@ import { Text, View } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+    state = {
+        albums: []
+    };
+
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(response));
+            .then(response => this.setState({ albums: response.data }));
     }
 
     render() {
+        console.log(this.state);
+
         return (
             <View>
                 <Text>Album List!!!</Text>
