deddeb1e9662f9cd318ae2d0799bf3e25cee61db
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-05 12:35:50 +0530

    create album list component (with hardcoded text for now)

---
+++ src/components/AlbumList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AlbumList = () => {
+    return (
+        <View>
+            <Text>Album List!!!</Text>
+        </View>
+    );
+};
+
+export default AlbumList;
