e6f740364994594454174bd03907de2c7bf11e32
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-06 09:11:47 +0530

    pass button text as children

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -38,7 +38,9 @@ const AlbumDetail = ({ album }) => {
             </CardSection>
 
             <CardSection>
-                <Button onPress={() => Linking.openURL(url)} />
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
