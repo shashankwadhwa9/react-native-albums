373bc1714151b16c08ac7b787bb137e35736daa9
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-06 08:46:28 +0530

    making the last cardsection (the button)

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
     const { title, artist, thumbnail_image, image } = album;
@@ -34,6 +35,10 @@ const AlbumDetail = ({ album }) => {
                     style={imageStyle}
                     source={{ uri: image }}
                 />
+            </CardSection>
+
+            <CardSection>
+                <Button />
             </CardSection>
         </Card>
     );
